8be532585ec8eaa36dcf922e665db96559a84532
Gipsys <458586540@qq.com>
2024-10-14 11:41:45 +0800

    新增插件

--- MyFrameWork/Assets/MyTools/Common/FormalEnv/CloseLog.cs
+++ MyFrameWork/Assets/MyTools/Common/FormalEnv/CloseLog.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CloseLog : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-
         Debug.unityLogger.logEnabled = false;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
